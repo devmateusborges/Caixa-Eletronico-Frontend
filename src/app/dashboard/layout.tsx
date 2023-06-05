986ce77a9f6dad1d82384b508c53721b6503294b
Mateus Borges <132221962+DevAcademico@users.noreply.github.com>
2023-06-05 16:28:25 -0300

    ajustes

--- src/app/dashboard/layout.tsx
+++ src/app/dashboard/layout.tsx
@@ -6,10 +6,10 @@ export default function RootLayout({ children }: { children: ReactNode }) {
   return (
     <>
       <div className="w-full h-screen flex flex-col md:flex-row gap-y-3 md:gap-x-3  md:p-3 bg-[#B5C2C9] items-center justify-center">
-        <div className="w-full h-full flex items-center justify-center">
+        <div className="w-full h-full md:hidden flex items-center justify-center">
           <p className="font-bold">Essa aplicação somente para computadores</p>
         </div>
-        <div className="hidden md:block w-full md:w-[18%] h-full ">
+        <div className="hidden md:block  w-full md:w-[18%] h-full ">
           <Menu />
         </div>
         <div className="hidden md:block h-full w-full md:w-[75%] ">
